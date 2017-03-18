ab517b76c2b6adf54543c68549e4b59bdaffb413
loversaber <851117342@qq.com>
2017-03-18 23:46:20 +0800

    learn urllib

---
+++ urllib2_learn.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import urllib.request
+
+request = urllib.request.Request("http://www.baidu.com")
+response = urllib.request.urlopen(request)
+html = response.read()
+print(html)
